217061f83741746d36e90c3546fe890baef41afc
Hanif Fadillah Amryudin <documenthanif16@gmail.com>
2022-08-31 22:19:12 +0700

    Initial Commit

---
+++ wheater/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wheater
+//
+//  Created by Hanif Fadillah Amrynudin on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wheater/wheaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheaterApp.swift
+//  wheater
+//
+//  Created by Hanif Fadillah Amrynudin on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct wheaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
